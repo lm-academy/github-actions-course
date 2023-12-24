cf081970eb63685adb901669a529b21a60440bc3
Lauro Fialho Mueller <contact@lauromueller.com>
2023-12-24 15:59:40 +0100

    feat(17-custom-actions): add output to docker action

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -2,6 +2,12 @@ import os
 import requests
 import time
 
+
+def set_output(file_path, key, value):
+    with open(file_path, 'a') as file:
+        print(f'{key}={value}', file=file)
+
+
 def ping_url(url, delay, max_trials):
     trials = 0
 
@@ -29,6 +35,8 @@ def run():
 
     website_reachable = ping_url(website_url, delay, max_trials)
 
+    set_output(os.getenv('GITHUB_OUTPUT'), 'url-reachable', website_reachable)
+    
     if not website_reachable:
         raise Exception(f"Website {website_url} is malformed or unreachable.")
 
