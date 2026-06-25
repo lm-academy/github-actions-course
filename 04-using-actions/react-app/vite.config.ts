e0238c2b8ecbc67cc41254b609db906f27f60d44
Lauro Fialho Müller <contact@lauromueller.com>
2026-06-25 12:17:39 +0200

    chore: migrate to Vite instead of CRA

---
+++ 04-using-actions/react-app/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './coverage',
+    },
+  },
+})
